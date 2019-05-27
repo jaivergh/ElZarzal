3f472ec5ce35e2350b16ace95d2b33a85c580c57
jcamilogh <jcamilogh@outlook.com>
2019-05-27 16:01:00 -0500

    Add project files.

---
+++ ReportesElZarzal/Inicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReportesElZarzal
+{
+	public partial class Inicio : Form
+	{
+		public Inicio()
+		{
+			InitializeComponent();
+		}
+
+		private void ordenDeProducciónToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			OrdenDeProduccionForm ordenDeProduccionForm = new OrdenDeProduccionForm();
+			ordenDeProduccionForm.Show();
+		}
+	}
+}
---
+++ ReportesElZarzal/OrdenDeProduccionForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReportesElZarzal
+{
+	public partial class OrdenDeProduccionForm : Form
+	{
+		public OrdenDeProduccionForm()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OrdenDeProduccionReport ordenDeProduccionReport = new OrdenDeProduccionReport();
+			ProcesosSubreport procesosSubreport = new ProcesosSubreport();
+			InformeComponentesSubreport informeComponentesSubreport = new InformeComponentesSubreport();
+
+			SqlConnection conProcesos = new SqlConnection();
+			conProcesos.ConnectionString = 
+		}
+	}
+}
